3b0b527768a83927eac46bf88e6349fdd631e395
DaniilStudenikin <daniil.evgenyevich703@gmail.com>
2023-04-03 12:12:48 +0300

    add products to show category

--- app/Http/Resources/Category/SingleCategoryResource.php
+++ app/Http/Resources/Category/SingleCategoryResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Category;
 
+use App\Http\Resources\Product\ProductResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,6 +18,7 @@ class SingleCategoryResource extends JsonResource
                 'name' => $this->name,
                 'description' => $this->description,
                 'parent_id' => $this->parent_id,
+                'products' => ProductResource::collection($this->products),
                 'products_count' => $this->products_count
             ]
         ];
