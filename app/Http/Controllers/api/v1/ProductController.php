da916aafd3a57969b9c6f1e912a5b7e3b6d6c4d8
fastrapier <fastrapier1@yandex.ru>
2023-04-22 00:21:06 +0300

    remove product update from unguarded

--- app/Http/Controllers/api/v1/ProductController.php
+++ app/Http/Controllers/api/v1/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
 {
     public function __construct(private readonly ProductService $productService)
     {
-        $this->middleware('auth.role:admin', ['except' => ['index', 'show', "update"]]);
+        $this->middleware('auth.role:admin', ['except' => ['index', 'show']]);
     }
 
     public function index()
