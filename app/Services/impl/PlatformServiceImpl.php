870f2ea937e37886ed582a02bd7000d23a8d88bc
fastrapier <fastrapier1@yandex.ru>
2023-09-02 00:53:16 +0500

    platform add update

--- app/Services/impl/PlatformServiceImpl.php
+++ app/Services/impl/PlatformServiceImpl.php
@@ -14,11 +14,34 @@ class PlatformServiceImpl implements PlatformService
     {
         foreach ($validated['platform'] as $elem)
         {
-            Platform::create([
-                'configuration_id' => $elem['configuration_id'],
-                'selected_product_id' => $elem['selected_product_id'],
-                'product_id' => $product->id
-            ]);
+            $configuration_id = $validated['configuration_id'];
+
+            $selected_product_id = $elem['selected_product_id'];
+
+            $found = false;
+
+            foreach ($product->platform as $plat)
+            {
+                if($plat->configuration_id == $configuration_id)
+                {
+                    $found = true;
+
+                    if($selected_product_id !== $plat->selected_product_id)
+                    {
+                        Platform::where(['configuration_id' => $plat->configuration_id, 'product_id' => $plat->product_id])->update(['selected_product_id' => $selected_product_id]);
+                    }
+
+                    break;
+                }
+            }
+            if(!$found)
+            {
+                Platform::create([
+                    'configuration_id' => $elem['configuration_id'],
+                    'selected_product_id' => $elem['selected_product_id'],
+                    'product_id' => $product->id
+                ]);
+            }
         }
 
         $product->load(['productType', 'platform', 'specification_values']);
